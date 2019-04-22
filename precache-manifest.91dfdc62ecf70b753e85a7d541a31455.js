3fe41073d20ec833f31b4e1ea7d1def452e85fea
zohaibdhukka <zohaibdhukka@gmail.com>
2019-04-22 18:28:19 -0500

    Updates

---
+++ precache-manifest.91dfdc62ecf70b753e85a7d541a31455.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "cab0dc886674421861b7",
+    "url": "/Clicky-Game/static/js/main.2504178d.chunk.js"
+  },
+  {
+    "revision": "2e75b5943902ac95e22d",
+    "url": "/Clicky-Game/static/js/2.c999b0f2.chunk.js"
+  },
+  {
+    "revision": "18ef9fc6bb039cb686de00ef89d9da1c",
+    "url": "/Clicky-Game/index.html"
+  }
+];
